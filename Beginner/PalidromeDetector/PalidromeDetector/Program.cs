80d0bf3358a732834c36173bc1dea5a6e2c021e3
Bailey Dauterman <baileydauterman@Baileys-Air.lan>
2023-01-03 20:01:16 -0500

    palidrome checker

---
+++ Beginner/PalidromeDetector/PalidromeDetector/Program.cs
@@ -0,0 +1,17 @@
+﻿if (args[0] is null)
+{
+    throw new ArgumentNullException(nameof(args));
+}
+
+var saveString = args[0].ToLower();
+
+var isPalidrome = saveString.Equals(ReverseString(saveString)) ? string.Empty : "not ";
+
+Console.WriteLine($"{args[0]} is {isPalidrome}a palidrome");
+
+string ReverseString(string str)
+{
+    var chars = str.ToLower().ToCharArray();
+    Array.Reverse(chars);
+    return new string(chars);
+}
