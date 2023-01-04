75c415d9e7be8e7b5adf25de5ff6d7df1a207495
Bailey Dauterman <baileydauterman@Baileys-Air.lan>
2023-01-03 19:34:56 -0500

    vowel counter

---
+++ Beginner/VowelCounter/VowelCounter/Program.cs
@@ -0,0 +1,13 @@
+﻿if (string.IsNullOrEmpty(args[0]))
+{
+    throw new Exception("Not a word");
+}
+
+var vowels = new List<char>
+{
+    'a', 'e', 'i', 'o', 'u', 'y'
+};
+
+var count = args[0].Length - args[0].Except(vowels).Count();
+
+Console.WriteLine($"{args[0]} contains {count} vowels");
